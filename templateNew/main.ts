38cb35e323ee09c95484e6b2d7e9d31f6caab4ea
vcheng <wutong9802@qq.com>
2025-07-09 23:30:38 +0800

    sub

--- templateNew/main.ts
+++ templateNew/main.ts
@@ -14,3 +14,13 @@ class Main {
 }
 
 new Main()
+
+
+// https://v2.itlao6.com/v2ray222
+// https://raw.githubusercontent.com/ripaojiedian/freenode/main/sub
+// https://raw.githubusercontent.com/eycorsican/rule-sets/master/kitsunebi_sub
+// https://shadowshare.v2cross.com/publicserver/servers/temp/lQ2u8C1GjJUgAIpt
+// https://www.stairnode.com/node/20241107-v2ray.txt
+// https://platform.djjc.cfd/api/v1/client/subscribe?token=5a1eed5977ba495aaf3c068a8b6573f6
+
+// https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2
